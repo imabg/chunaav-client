230550bb0817e956f27c216fdbcb25ff9416c2f1
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-13 19:28:06 +0530

    add admin_token

--- src/containers/Admin/Login/saga.js
+++ src/containers/Admin/Login/saga.js
@@ -3,13 +3,13 @@ import { ADMIN_LOGIN_REQUESTED, ADMIN_LOGIN_SUCCESSFULLY } from "./types";
 
 import loginAdmin from "../../../API";
 
-
 function* adminLogin(action) {
   try {
     const admin = yield call(loginAdmin, action.payload);
     yield put({ type: ADMIN_LOGIN_SUCCESSFULLY, admin });
     if (admin.success) {
       localStorage.setItem("admin", true);
+      localStorage.setItem("admin_token", admin.data.token);
     }
   } catch (error) {
     console.log(error);
