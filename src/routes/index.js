2cc79b8987bf9a1252c2717b09e313247dfe68c6
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-14 19:25:25 +0530

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoutes";
 
 import AdminLogin from "../containers/Admin/Login";
+import Dashboard from "../containers/Admin/Dashboard";
+import Voter from "../containers/Admin/Dashboard/voter"
 
 const CustomRoutes = () => {
   return (
     <Switch>
-      {/* <PrivateRoute path="/admin/login" exact component={AdminLogin} /> */}
+      <PrivateRoute path="/admin/dashboard" exact component={Dashboard} />
+      <PrivateRoute path="/admin/voters" exact component={Voter} />
       <Route path="/admin/login" exact component={AdminLogin} />
     </Switch>
   );
