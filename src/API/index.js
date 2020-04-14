ed5dabaee137eac0f161c83baeb4beb1f2899394
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-14 19:26:56 +0530

    update API

--- src/API/index.js
+++ src/API/index.js
@@ -2,10 +2,40 @@ import axios from "axios";
 
 const URL = `http://localhost:${process.env.REACT_APP_SERVER_PORT}`;
 
-const loginAdmin = (credentials) => {
-  return axios.post(`${URL}/api/admin/login`, credentials).then((res) => {
-    return res.data;
-  });
+export const loginAdmin = async (credentials) => {
+  try {
+    const response = await axios.post(`${URL}/api/admin/login`, credentials);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
 };
 
-export default loginAdmin;
+export const fetchAdminDetails = async () => {
+  try {
+    let adminToken = localStorage.getItem("admin_token");
+    let adminConfig = {
+      headers: { Authorization: `Bearer ${adminToken}` },
+    };
+    const res = await axios.get(`${URL}/api/admin/details`, adminConfig);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchVoterInfo = async (aadhar_num) => {
+  try {
+    let adminToken = localStorage.getItem("admin_token");
+    let adminConfig = {
+      headers: { Authorization: `Bearer ${adminToken}` },
+    };
+    const res = await axios.get(
+      `${URL}/api/admin/voter/details?aadhar_num=${aadhar_num}`,
+      adminConfig
+    );
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
