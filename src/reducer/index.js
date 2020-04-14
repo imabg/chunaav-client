2b56fb6a0c3c6327e25b622ea95c224e8a5cf6fd
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-14 19:25:53 +0530

    add dashboard reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from "redux";
-import adminLoginReducer from "../containers/Admin/Login/reducer";
 
-const RootReducer = combineReducers({ admin: adminLoginReducer });
+import adminLoginReducer from "../containers/Admin/Login/reducer";
+import dashboardReducer from "../containers/Admin/Dashboard/reducer";
+
+const RootReducer = combineReducers({
+  admin: adminLoginReducer,
+  dashboard: dashboardReducer,
+});
 
 export default RootReducer;
