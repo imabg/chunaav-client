a58101a535ad504dc8b70b5cb953afbf8cb31d43
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-14 19:29:41 +0530

    update saga

--- src/containers/Admin/Login/saga.js
+++ src/containers/Admin/Login/saga.js
@@ -1,7 +1,12 @@
 import { call, put, takeLatest } from "redux-saga/effects";
-import { ADMIN_LOGIN_REQUESTED, ADMIN_LOGIN_SUCCESSFULLY } from "./types";
+import {
+  ADMIN_LOGIN_REQUESTED,
+  ADMIN_LOGIN_SUCCESSFULLY,
+  ADMIN_DETAILS_SUCCESS,
+  ADMIN_DETAILS_REQUESTED,
+} from "./types";
 
-import loginAdmin from "../../../API";
+import { loginAdmin, fetchAdminDetails } from "../../../API";
 
 function* adminLogin(action) {
   try {
@@ -11,13 +16,25 @@ function* adminLogin(action) {
       localStorage.setItem("admin", true);
       localStorage.setItem("admin_token", admin.data.token);
     }
+    const adminDetails = yield call(fetchAdminDetails);
+    yield put({ type: ADMIN_DETAILS_SUCCESS, adminDetails });
   } catch (error) {
     console.log(error);
   }
 }
 
+// function* adminDetails() {
+//   try {
+//     const admin = yield call(fetchAdminDetails);
+//     yield put({ type: ADMIN_DETAILS_SUCCESS, admin });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
 function* adminLoginSaga() {
   yield takeLatest(ADMIN_LOGIN_REQUESTED, adminLogin);
+  // yield takeLatest(ADMIN_DETAILS_REQUESTED, adminDetails);
 }
 
 export default adminLoginSaga;
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,8 @@
 import { all } from "redux-saga/effects";
 
 import adminLoginSaga from "../containers/Admin/Login/saga";
+import dashboardReducer from "../containers/Admin/Dashboard/saga";
 
 export default function* rootSaga() {
-  yield all([adminLoginSaga()]);
+  yield all([adminLoginSaga(), dashboardReducer()]);
 }
