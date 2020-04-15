647a13520e3ec4764b86b2c2c62a784fec316022
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-15 23:43:51 +0530

    update navBar

--- src/containers/Admin/Dashboard/navbar/index.js
+++ src/containers/Admin/Dashboard/navbar/index.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { AppBar, Toolbar, Button, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-export default function ButtonAppBar() {
+export default function ButtonAppBar(props) {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">
-            <Link to="/admin/dashboard" className="text-white">Admin Dashboard </Link>👮
+            <Link to="/admin/dashboard" className="text-white">
+              Admin Dashboard{" "}
+            </Link>
+            👮
           </Typography>
           <Link to="/admin/voters" className="ml-4 text-white">
             VOTER
           </Link>
           <Link to="/admin/candidate" className="ml-4 text-white">
             CANDIDATE
-          </Link>
-          <Link to="/admin/result" className="ml-4 text-white">
-            Result
           </Link>
         </Toolbar>
       </AppBar>
