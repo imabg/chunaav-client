6488f00ac8f81c3a87723fe92bbf0c7b8ddff120
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-16 00:06:29 +0530

    update route for candidate

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,14 @@ import PrivateRoute from "./PrivateRoutes";
 import AdminLogin from "../containers/Admin/Login";
 import Dashboard from "../containers/Admin/Dashboard";
 import Voter from "../containers/Admin/Dashboard/voter"
+import Candidate from "../containers/Admin/Dashboard/candidate"
 
 const CustomRoutes = () => {
   return (
     <Switch>
       <PrivateRoute path="/admin/dashboard" exact component={Dashboard} />
       <PrivateRoute path="/admin/voters" exact component={Voter} />
+      <PrivateRoute path="/admin/candidates" exact component={Candidate} />
       <Route path="/admin/login" exact component={AdminLogin} />
     </Switch>
   );
