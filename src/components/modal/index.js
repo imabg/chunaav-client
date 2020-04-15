e4c0b45a81138004eb7d181afa1e8c768971a901
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-15 23:41:29 +0530

    :fire: update custom modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,41 +1,31 @@
 import React from "react";
-import { Modal, Fade, Backdrop } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import Modal from "react-modal";
 
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 const CustomModal = (props) => {
-  const classes = useStyles();
   return (
     <div>
       <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={props.isOpen}
-        onClose={props.closeModal}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
+        isOpen={props.isOpen}
+        ariaHideApp={false}
+        onRequestClose={props.closeModal}
+        style={customStyles}
+        shouldCloseOnEsc={true}
+        shouldCloseOnOverlayClick
       >
-        <Fade in={props.isOpen}>
-          <div className={classes.paper}>{props.children}</div>
-        </Fade>
+        <div>{props.heading}</div>
+        {props.children}
       </Modal>
     </div>
   );
