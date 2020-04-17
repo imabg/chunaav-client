04754dc3db8272a159221a20e2632e23458156f7
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-17 23:49:46 +0530

    updated routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,19 +2,34 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import PrivateRoute from "./PrivateRoutes";
+import PrivateRouteVoter from "./PrivateRouteVoter"
 
 import AdminLogin from "../containers/Admin/Login";
 import Dashboard from "../containers/Admin/Dashboard";
 import Voter from "../containers/Admin/Dashboard/voter"
 import Candidate from "../containers/Admin/Dashboard/candidate"
 
+// Voter Routes
+import VoterLogin from "../containers/Voter/Login"
+import OTP from "../containers/Voter/OTP"
+import VotingScreen from "../containers/Voter/VotingScreen"
+import ThankU from "../containers/Voter/ThankU"
+
+// NoMatch -> 404
+import NoMatch from "../components/NoMatch";
+
 const CustomRoutes = () => {
   return (
     <Switch>
+      <Route path="/admin/login" exact component={AdminLogin} />
       <PrivateRoute path="/admin/dashboard" exact component={Dashboard} />
       <PrivateRoute path="/admin/voters" exact component={Voter} />
       <PrivateRoute path="/admin/candidates" exact component={Candidate} />
-      <Route path="/admin/login" exact component={AdminLogin} />
+      <Route path="/" exact component={VoterLogin} />
+      <PrivateRouteVoter path="/otp" exact component={OTP} />
+      <PrivateRouteVoter path="/screen" exact component={VotingScreen} />
+      <PrivateRouteVoter path="/thankU" exact component={ThankU} /> 
+      <Route component={NoMatch} />
     </Switch>
   );
 };
