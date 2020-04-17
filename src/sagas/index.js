d708d7a471797804f26a436ca300bfd0166fcd4c
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-17 23:49:29 +0530

    enabled voterSaga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,7 +2,8 @@ import { all } from "redux-saga/effects";
 
 import adminLoginSaga from "../containers/Admin/Login/saga";
 import dashboardReducer from "../containers/Admin/Dashboard/saga";
+import voterSaga from "../containers/Voter/saga";
 
 export default function* rootSaga() {
-  yield all([adminLoginSaga(), dashboardReducer()]);
+  yield all([adminLoginSaga(), dashboardReducer(), voterSaga()]);
 }
